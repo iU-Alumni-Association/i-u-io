7b44720ac7624a3b3c590ee5f871f41cf3c9fccd
small-engineer <wakamiya626@gmail.com>
2025-01-26 02:40:56 +0900

    change contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./assets/js/**/*.js",
     "./themes/**/*.{html,js}",
     "./admin/**/*.{html,js,jsx}",
-    "./contact/**/*.{html,js,jsx}",
   ],
   theme: {
     extend: {
